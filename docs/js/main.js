b944a67b3d764ad49dad032b367e007793c48ca6
kasiforce <2819407652@qq.com>
2025-11-26 01:05:03 +0000

    Github Action Automatic Update Arxiv Papers

--- docs/js/main.js
+++ docs/js/main.js
@@ -309,11 +309,10 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
         // 更新benchmark按钮的数量
-        //updateBenchmarkButtonCounts();
+        updateBenchmarkButtonCounts();
 
         // 更新研究领域按钮的数量
         updateCategoryButtonCounts();
-        updateBenchmarkButtonCounts();
 
         // 更新显示
         if (resultsCount) {
